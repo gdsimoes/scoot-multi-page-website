3ad99dc693edc3379f95827a8419a4b47ba7ce2c
Guilherme Dias Simoes <gds.simoes@gmail.com>
2023-03-17 15:35:14 -0400

    Add API route

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,3 @@
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+    return new Response("This is an easter egg! I just wanted to test how Route Handlers work in Next.js 13...");
 }
